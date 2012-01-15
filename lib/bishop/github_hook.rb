2a0268b6883d9b2973b06c0589d0e02445142647
Tonni Tølbøll Lund Aagesen <tonni@toelboell-lund.dk>
2012-01-15 22:35:57 +0100

    Process actual data from Github instead of test file

--- lib/bishop/github_hook.rb
+++ lib/bishop/github_hook.rb
@@ -1,11 +1,12 @@
 # -*- mode: ruby; tab-width: 2; indent-tabs-mode: nil; -*-
+require "uri"
 require "json"
 
 module Bishop
   # see http://help.github.com/post-receive-hooks/
   class GithubHook < Bishop::Base
     post "/#{ENV["BISHOP_API_KEY"]}" do
-      payload  = JSON.parse request.body.read
+      payload  = JSON.parse(URI.unescape(params["payload"]))
       channels = ENV["BISHOP_GITHUB_HOOK_CHANNELS"].split(",")
       
       payload["commits"].each do |commit|
