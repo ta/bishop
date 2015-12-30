386f63d2e7b7a20c9be4cfa1e05aac26022740e0
Tonni Tølbøll Lund Aagesen <tonni@toelboell-lund.dk>
2015-12-30 19:07:53 +0000

    refactor to use class instance variables + cleanup

--- lib/bishop/bot.rb
+++ lib/bishop/bot.rb
@@ -8,64 +8,61 @@ require "lib/bishop/bot/seen"
 
 module Bishop
   class Bot
-    DRBURI      = "drbunix:./tmp/bishop.sock"
-    @@proc      = nil
-    @@instance  = nil
-    
-    def self.instance
-      self.start unless @@instance
-      @@instance
-    end
-    
-    def self.start
-      return @@instance if @@instance
-      
-      @@proc = fork do
-        bot = Cinch::Bot.new do
-          configure do |c|
-            c.nick            = "bishop"
-            c.user            = "bishop"
-            c.realname        = "Bishop"
-            c.server          = ENV["BISHOP_SERVER"]
-            c.port            = ENV["BISHOP_PORT"] || !!ENV["BISHOP_SSL_USE"] ? 6697 : 6667
-            c.channels        = ENV['BISHOP_CHANNELS'].to_s.split(",")
-            c.ssl.use         = !!ENV["BISHOP_SSL_USE"]
-            c.ssl.verify      = !!ENV["BISHOP_SSL_VERIFY"]
-            c.verbose         = !!ENV["BISHOP_LOG_VERBOSE"]
-            c.plugins.prefix  = /^bishop[\:,] /
-            c.plugins.plugins = [Commands,Greetings,Seen]
+    DRBURI    = "drbunix:/tmp/bishop.sock"
+    @pid      = nil
+    @instance = nil
+
+    class << self
+      def start
+        return @instance if @instance
+
+        @pid = fork do
+          bot = Cinch::Bot.new do
+            configure do |c|
+              c.nick            = "bishop"
+              c.user            = "bishop"
+              c.realname        = "Bishop"
+              c.server          = ENV["BISHOP_SERVER"]
+              c.port            = (ENV["BISHOP_PORT"] || ENV.key?("BISHOP_SSL_USE")) ? 6697 : 6667
+              c.channels        = ENV['BISHOP_CHANNELS'].to_s.split(",")
+              c.ssl.use         = ENV.key?("BISHOP_SSL_USE")
+              c.ssl.verify      = ENV.key?("BISHOP_SSL_VERIFY")
+              c.verbose         = ENV.key?("BISHOP_LOG_VERBOSE")
+              c.plugins.prefix  = /^bishop[\:,] /
+              c.plugins.plugins = [Commands, Greetings, Seen]
+            end
+
+            Signal.trap("INT") do
+              bot.quit
+              DRb.stop_service
+            end
+
+            Signal.trap("TERM") do
+              bot.quit
+              DRb.stop_service
+            end
           end
-          
-        end
-        
-        log = File.open("./tmp/bishop.log", "a")
-        log.sync = true
-        bot.loggers << Cinch::Logger::FormattedLogger.new(log)
-        
-        Signal.trap("INT") do
-          puts "*** bishop is shutting down (PID #{Process.pid})"
-          bot.quit "I'll go! Believe me, I'd prefer not to. I may be synthetic, but I'm not stupid"
-          DRb.stop_service
-        end
-        
-        Signal.trap("TERM") do
-          puts "*** bishop is shutting down (PID #{Process.pid})"
-          bot.quit "I'll go! Believe me, I'd prefer not to. I may be synthetic, but I'm not stupid"
-          DRb.stop_service
+
+          log = File.open("./tmp/bishop.log", "a")
+          log.sync = true
+          bot.loggers << Cinch::Logger::FormattedLogger.new(log)
+
+          DRb.start_service(DRBURI, bot)
+          bot.start
         end
 
-        DRb.start_service(DRBURI, bot)
-        bot.start()
+        sleep 2
+        DRb.start_service
+        @instance = DRbObject.new_with_uri(DRBURI)
       end
-      sleep 2
-      DRb.start_service
-      @@instance = DRbObject.new_with_uri(DRBURI)
+
+      def stop
+        puts "*** bishop is shutting down (PID #{Process.pid})"
+        @instance.quit "I'll go! Believe me, I'd prefer not to. I may be synthetic, but I'm not stupid"
+        @instance = nil
+        DRb.stop_service
+        Process.kill("INT", @pid)
+      end
     end
-    
-    def self.stop
-      Process.kill("INT", @@proc)
-    end
-    
   end
 end
-
